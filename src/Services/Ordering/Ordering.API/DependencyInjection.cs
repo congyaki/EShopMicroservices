ff16e98bcc8b9cd5c79a52637f7faa0eb48553b0
congyaki <duccong29092003@gmail.com>
2024-09-16 23:15:00 +0700

    Feat (Ordering): Add Dependency Injections between Layers.

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.Exceptions.Handler;
+
+namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices (this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+
+        public static WebApplication UseApiServices (this WebApplication app)
+        {
+            app.UseMiddleware<CustomExceptionHandler>();
+
+            return app;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,6 +19,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseApiServices();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
