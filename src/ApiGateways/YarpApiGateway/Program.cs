714ade49a6ab18607991c1a76836e443715891b0
congyaki <duccong29092003@gmail.com>
2024-10-23 22:50:19 +0700

    Feat (ApiGateway): Completed Implement API Gateway using Yarp Reverse Proxy.

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,6 +1,27 @@
+﻿using AspNetCoreRateLimit;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Cấu hình YARP
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Thêm cấu hình Rate Limiting
+builder.Services.AddOptions();
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.AddInMemoryRateLimiting();
+
+// Thêm dịch vụ xử lý IP Rate Limiting
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Sử dụng Middleware IP Rate Limiting
+app.UseIpRateLimiting();
+
+// Thêm Reverse Proxy
+app.MapReverseProxy();
 
 app.Run();
