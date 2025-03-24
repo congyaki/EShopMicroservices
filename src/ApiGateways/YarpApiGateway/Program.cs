8dbbc13ab9f9b3bddf0348b348f9e0022cc1a6b4
congyaki <duccong29092003@gmail.com>
2025-03-24 16:26:42 +0700

    Fix YARP Gateway.

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,4 +1,5 @@
-﻿using AspNetCoreRateLimit;
+﻿
+using AspNetCoreRateLimit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,21 +8,23 @@ builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 // Thêm cấu hình Rate Limiting
-builder.Services.AddOptions();
-builder.Services.AddMemoryCache();
-builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
-builder.Services.AddInMemoryRateLimiting();
-
-// Thêm dịch vụ xử lý IP Rate Limiting
-builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+//builder.Services.AddOptions();
+//builder.Services.AddMemoryCache();
+//builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+//builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+//builder.Services.AddInMemoryRateLimiting();
+//builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
 
 var app = builder.Build();
 
 // Sử dụng Middleware IP Rate Limiting
-app.UseIpRateLimiting();
+//app.UseIpRateLimiting();
 
 // Thêm Reverse Proxy
 app.MapReverseProxy();
+
+// Lỗi là do thằng UseRouting này
+//app.UseRouting();
 
 app.Run();
