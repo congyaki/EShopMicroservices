6b8c7a3edd33fd859c9100f022a58c63464ed61a
congyaki <duccong29092003@gmail.com>
2025-04-21 17:24:45 +0700

    Update Docker Compose configurations and scripts
    Changed restart policy for services in docker-compose.override.yml from 'always' to 'on-failure' for better control. Added profiles for services in docker-compose.yml to enable selective service startup. Updated run-gateway.bat to incorporate new profiles for launching services based on user input.

--- src/run-gateway.bat
+++ src/run-gateway.bat
@@ -11,7 +11,6 @@ if "%choice%"=="1" docker-compose --profile ocelot up -d
 if "%choice%"=="2" docker-compose --profile yarp up -d
 if "%choice%"=="3" docker-compose --profile kong up -d
 if "%choice%"=="4" docker-compose --profile traefik up -d
-if "%choice%"=="5" docker-compose --profile tyk up -d
 
 echo.
 echo API Gateway started successfully.
